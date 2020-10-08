fcb0d54033e1bd68845f4c413ba2a2446e432704
BenSymons <bsymonsb@hotmail.co.uk>
2020-10-08 16:30:06 +0100

    do add task with buttons

--- todo_list/src/AddTask.jsx
+++ todo_list/src/AddTask.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 
-const AddTask = (props) => {
-    return (
-        <form>
-            <label>Enter New Task:</label>
-            <input type="text" placeholder="Enter your task here..."></input>
-        </form>
-    )
+class AddTask extends React.Component {
+    state = {
+        newTask: ""
+    }
+
+    handleChange = (event) => {
+        const newTask = event.target.value
+        this.setState(() => {
+            return { newTask } // Object destructuring
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit={(event) => {
+                const task = this.state.newTask
+                event.preventDefault()
+                this.props.addTask(task)
+            }}>
+                <label>Enter New Task:</label>
+                <input onChange={this.handleChange} type="text" placeholder="Enter your task here..."></input>
+                <button>Add task</button>
+            </form>
+        )
+    }
 }
 
 export default AddTask;
--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -4,14 +4,27 @@ import NameEntry from "./NameEntry"
 import AddTask from "./AddTask"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <NameEntry/>
-        <AddTask/>
-        <ToDoList/>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    tasks: []
+  }
+
+  addTask = (task) => {
+    this.setState((previousState) => {
+      return { tasks: [...previousState.tasks, task] }
+    })
+  }
+  render() {
+    // console.log(this.state);
+    return (
+      <div className="App">
+        <NameEntry />
+        <AddTask addTask={this.addTask} />
+        <ToDoList tasks={this.state.tasks} />
+      </div>
+    );
+  }
 }
 
 export default App;
