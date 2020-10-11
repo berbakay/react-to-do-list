ae57388e619c6e536d13321bbd991eb5f18e7e6b
berbakay <rick.kay90@gmail.com>
2020-10-11 18:06:37 +0100

    Updates

---
+++ precache-manifest.b5512127724cacb21d9e20491a527742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d2157734aa13cdd8469ac43f394c4a",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "245d473b377dea9b6322",
+    "url": "/react-to-do-list/static/css/main.18dd109b.chunk.css"
+  },
+  {
+    "revision": "d6cfee1714e1ac983168",
+    "url": "/react-to-do-list/static/js/2.85913005.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do-list/static/js/2.85913005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "245d473b377dea9b6322",
+    "url": "/react-to-do-list/static/js/main.a80d5228.chunk.js"
+  },
+  {
+    "revision": "80ace135046b4f21cfe4",
+    "url": "/react-to-do-list/static/js/runtime-main.9bfb89f0.js"
+  }
+]);
